8ed0e026ff51494ca97b078c18664c794c38e4cf
Colin Canaday <cjcanaday@gmail.com>
2024-09-10 21:16:45 +0200

    Added input to SearchBar

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,21 +1,33 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
 
 export default function SearchBar() {
 	const [majors, setMajors] = useState();
-	const [value, setValue] = useState<string>("");
 	const [suggestions, setSuggestions] = useState<string[]>([]);
+    const [value, setValue] = useState<string>("");
+
+    const inputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+
+        const target = e.target;
+
+        if(target) setValue(target.value.toLowerCase());
+    }
 
 	useEffect(() => {
 		axios.get("/src/data/getafe-data.json").then((major) => {
 			setMajors(major.data);
 			console.log(major.data);
 			console.log(major.data['Doble Grado en Ciencias Políticas y Sociología']['10065']['groups']['Grupo 1']['schedule'][0]['day'])
-		});
+		}).catch( (err) => {
+            console.log(err);
+        });
 	}, []);
+
+
 
 	return (
 		<div>
+            <input onChange={inputHandler} value={value} placeholder="Search for classes" type="text"/>
 		</div>
 	);
 }
